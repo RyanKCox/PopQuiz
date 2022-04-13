e555c30d328be5ce3a1f755becb5310e0ee7f508
sarahkorrin <sarahkorrin@tutanota.com>
2022-04-13 16:05:44 -0400

    More work on the UI for the Search Quizzes + Saved Quizzes Screens

--- app/src/main/java/com/revature/popquiz/view/shared/Shared.kt
+++ app/src/main/java/com/revature/popquiz/view/shared/Shared.kt
@@ -9,9 +9,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import java.time.format.TextStyle
 
 /**
  * Shared Composables
@@ -70,16 +73,60 @@ fun QuizCard()
         shape = RoundedCornerShape(25.dp)
     )
     {
-        Row(verticalAlignment = Alignment.Top)
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
         {
-
+            Text(
+                text = "Quiz Title",
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.absolutePadding(top = 10.dp)
+            )
         }
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Text(
+                text = "Short quiz description",
+                textAlign = TextAlign.Center
+            )
+        }
+
+        Row(
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.absolutePadding(bottom = 10.dp)
+        )
+        {
+            Text(
+                text = "Tags: ",
+                textAlign = TextAlign.Center
+            )
+            // Insert tags here
+
+            val tags: Set<String>
+            tags = setOf(" Language", " Topic", " Quiz type")
+
+            Text(
+                text = tags.toString(),
+                textAlign = TextAlign.Center
+            )
+        }
+
     }
 }
+
+
+
 
 @Preview
 @Composable
 fun ViewQuizCard()
 {
-
+    QuizCard()
 }
