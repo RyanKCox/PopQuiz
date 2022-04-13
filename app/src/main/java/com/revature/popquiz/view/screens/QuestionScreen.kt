08efdceea415b1890df677c9ec0dae1fb10647f7
Evan Jones <eljones02@student.ysu.edu>
2022-04-13 14:30:32 -0400

    QuestionScreen Begin

---
+++ app/src/main/java/com/revature/popquiz/view/screens/QuestionScreen.kt
@@ -0,0 +1,21 @@
+package com.revature.popquiz.view.screens
+
+import android.widget.ProgressBar
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.revature.popquiz.R
+import com.revature.popquiz.view.shared.QuizScaffold
+
+@Composable
+fun QuestionScreen(navController: NavController) {
+
+    QuizScaffold(sTitle = R.string.quiz_name.toString(), navController = navController) {
+
+        Column(modifier = Modifier.fillMaxSize()) {
+            ProgressBar(modifier = Modifier)
+        }
+    }
+}
