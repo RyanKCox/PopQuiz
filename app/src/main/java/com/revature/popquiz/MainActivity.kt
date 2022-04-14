36483c62597fc9c15bc43d59a2def32a3eeeee85
Evan Jones <eljones02@student.ysu.edu>
2022-04-14 11:35:51 -0400

    Splash Screen

--- app/src/main/java/com/revature/popquiz/MainActivity.kt
+++ app/src/main/java/com/revature/popquiz/MainActivity.kt
@@ -3,23 +3,31 @@ package com.revature.popquiz
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.revature.popquiz.ui.theme.PopQuizTheme
 import com.revature.popquiz.view.navigation.StartNav
+import com.revature.popquiz.viewmodels.SplashScreenViewModel
 
 class MainActivity : ComponentActivity() {
 
+    //Will change with Dagger-Hilt? -Evan
+    private val splashScreenViewModel: SplashScreenViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Used to install and modify the Splash screen -Evan
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                splashScreenViewModel.isLoading.value
+            }
+        }
 
         setContent {
 
