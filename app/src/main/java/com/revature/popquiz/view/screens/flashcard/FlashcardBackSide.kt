8a456cd1fff5bfcd65f7240b42586a5ea3506b0f
Evan Jones <eljones02@student.ysu.edu>
2022-04-14 16:55:06 -0400

    FlashcardBackSide

--- app/src/main/java/com/revature/popquiz/view/screens/flashcard/FlashcardBackSide.kt
+++ app/src/main/java/com/revature/popquiz/view/screens/flashcard/FlashcardBackSide.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -17,77 +16,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 
-@Composable
-fun FlashCardFrontSide(flipController: FlippableController) {
-    Box(modifier = Modifier) {
-        Surface(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth()
-                .height(220.dp),
-            shape = RoundedCornerShape(25.dp),
-            color = MaterialTheme.colors.secondaryVariant,
-            elevation = 16.dp
-        ) {
-            Box(modifier = Modifier.fillMaxSize()) {
-                ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-                    val (txtShuffleWord, txtWord, IblTapToFlip) = createRefs()
-                    
-                    Text(
-                        text = "This is an example of Flashcard text.",
-                        style = MaterialTheme.typography.h5,
-                        fontWeight = FontWeight.SemiBold,
-                        modifier = Modifier
-                            .constrainAs(txtWord) {
-                                linkTo(
-                                    start = parent.start,
-                                    end = parent.end
-                                )
-                                linkTo(
-                                    top = parent.top,
-                                    bottom = parent.bottom
-                                )
-                            }
-
-                    )
-                    Text(
-                        text = "Tap to flip",
-                        style = MaterialTheme.typography.subtitle2,
-                        color = Color.DarkGray,
-                        maxLines = 1,
-                        modifier = Modifier
-                            .constrainAs(IblTapToFlip) {
-                                linkTo(
-                                    start = parent.start,
-                                    end = parent.end
-                                )
-                                bottom.linkTo(parent.bottom)
-                            }
-                            .padding(bottom = 8.dp)
-                    )
-
-                    Button(
-                        onClick = { flipController.flip() },
-                        modifier = Modifier
-                            .constrainAs(txtShuffleWord) {
-                                top.linkTo(parent.top)
-                                end.linkTo(parent.end)
-                            }
-                            .padding(end = 8.dp)
-                    ) {
-                        Text(
-                            text = "Show definition",
-                            style = MaterialTheme.typography.subtitle2,
-                            color = Color.Black,
-                            maxLines = 1
-                        )
-                    }
-                }
-
-            }
-        }
-    }
-}
 @Composable
 fun FlashCardBackSide(flipController: FlippableController) {
     Box(modifier = Modifier) {
