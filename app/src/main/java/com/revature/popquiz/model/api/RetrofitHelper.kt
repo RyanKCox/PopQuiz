e88359e9d5a82ac89b9b4a93d7d9286ba17c6dc4
Evan Jones <eljones02@student.ysu.edu>
2022-04-15 14:41:45 -0400

    DnsSelector

--- app/src/main/java/com/revature/popquiz/model/api/RetrofitHelper.kt
+++ app/src/main/java/com/revature/popquiz/model/api/RetrofitHelper.kt
@@ -1,6 +1,13 @@
 package com.revature.popquiz.model.api
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.revature.popquiz.model.api.services.QuizApiService
+import com.revature.popquiz.model.dataobjects.Quiz
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 /**
  * Helper class for loading from a server
@@ -10,9 +17,6 @@ import retrofit2.Retrofit
 object RetrofitHelper {
 
     private val retrofit: Retrofit
-        get() {
-            TODO()
-        }
 
     init {
         /**
@@ -23,7 +27,38 @@ object RetrofitHelper {
          * CallAdapter to include Coroutine functionality into loading
          */
         val builder = Retrofit.Builder()
+            .baseUrl(NetConstants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+
+        /**
+         * Logging Interceptor:
+         * Adds an interceptor to the OkHttp client and allows for logs to be created
+         * during server connection
+         */
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        /**
+         * OkHttp client using the interceptor to add logging of timeout to server
+         */
+        val OkHttpClient = OkHttpClient.Builder()
+            .writeTimeout(0, TimeUnit.MILLISECONDS)
+            .writeTimeout(30, TimeUnit.MILLISECONDS)
+            .dns(DnsSelector())
+            .addInterceptor(loggingInterceptor).build()
+
+        /**
+         * Builds the retrofit object using the OkHttp client and builder
+         */
+        retrofit = builder.client(OkHttpClient).build()
     }
 
+    /**
+     * Retrieves the QuizApiService for API use
+     */
+    fun getAllQuizzesServices(): QuizApiService {
+        return retrofit.create(QuizApiService::class.java)
+    }
 
 }
