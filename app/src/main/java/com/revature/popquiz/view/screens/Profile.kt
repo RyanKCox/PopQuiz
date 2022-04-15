50b42bcb632f97e01b0bc127277fe6f29a78141e
jacobedens <100707183+jacobedens@users.noreply.github.com>
2022-04-15 10:39:23 -0500

    Updated profile and quiz finished UI

--- app/src/main/java/com/revature/popquiz/view/screens/Profile.kt
+++ app/src/main/java/com/revature/popquiz/view/screens/Profile.kt
@@ -3,145 +3,180 @@ package com.revature.popquiz.view.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.revature.popquiz.R
+import com.revature.popquiz.ui.theme.revBlue
 
 @Composable
 fun profile(/*navController: NavController*/) {
+    val scaffoldState = rememberScaffoldState()
     val context = LocalContext.current
-    Card(
-        modifier = Modifier.fillMaxSize(),
-        shape = RoundedCornerShape(25.dp)
-    ) {
-        Surface()
+    Scaffold(
+        backgroundColor = revBlue,
+        topBar = {/*header*/ },
+        scaffoldState = scaffoldState
+    )
+    {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        )
         {
+            LazyRow(modifier = Modifier.fillMaxWidth()) {
+                item() {
+                    Spacer(modifier = Modifier.fillParentMaxWidth(0.05F))
+                    Column(modifier = Modifier.fillParentMaxWidth(0.9F)) {
 
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            )
-            {
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(15.dp)
-                        .clickable { },
-                    elevation = 10.dp
-                ) {
-                    Column(
-                        modifier = Modifier.padding(15.dp)
-                    ) {
-                        Row {
-                            Image(
-                                painter = painterResource(id = R.drawable.clipart_3418189__340),
-                                contentDescription = "",
-                                modifier = Modifier
-                                    .height(150.dp)
-                                    .width(150.dp)
-                                    .fillMaxWidth(),
-                                contentScale = ContentScale.FillBounds
+
+                        Card(
+                            shape = RoundedCornerShape(25.dp),
+                            elevation = 50.dp,
+                            modifier = Modifier
+                                .fillMaxHeight(0.9F)
+                                .fillMaxWidth()
+                        ) {
+                            Column(
+                                modifier = Modifier.fillMaxSize(fraction = 0.9F),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Top
                             )
-                            Text(
-                                ("Name \n" +
-                                        " Your Overall Score \n" +
-                                        " Pop! Quiz Score \n" +
-                                        " Quizzes Completed ")
-                            )
+                            {
+                                Text(
+                                    text = "Profile",
+                                    fontSize = 30.sp,
+                                    fontWeight = FontWeight.Medium,
+                                    modifier = Modifier.padding(20.dp)
+                                )
+                                Spacer(modifier = Modifier.height(20.dp))
+                                Card(
+                                    backgroundColor = revBlue,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(15.dp)
+                                        .clickable { },
+                                    elevation = 10.dp,
+                                ) {
+                                    Column(
+                                        modifier = Modifier.padding(15.dp)
+                                    ) {
+                                        Row {
+                                            Image(
+                                                painter = painterResource(id = R.drawable.clipart_3418189__340),
+                                                contentDescription = "",
+                                                modifier = Modifier
+                                                    .height(150.dp)
+                                                    .width(150.dp)
+                                                    .fillMaxWidth(),
+                                                contentScale = ContentScale.FillBounds
+                                            )
+                                            Text(
+                                                ("Name \n" +
+                                                        " Your Overall Score \n" +
+                                                        " Pop! Quiz Score \n" +
+                                                        " Quizzes Completed ")
+                                            )
+                                        }
+                                    }
+                                }
+
+                                Card(
+                                    backgroundColor = revBlue,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(15.dp)
+                                        .clickable { },
+                                    elevation = 10.dp
+                                ) {
+                                    Column(
+                                        modifier = Modifier.padding(15.dp)
+                                    ) {
+                                        Text(
+                                            ("About me / subject interests \n" +
+                                                    " \n" +
+                                                    " \n" +
+                                                    "  ")
+                                        )
+                                    }
+                                }
+                                Card(
+                                    backgroundColor = revBlue,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(15.dp)
+                                        .clickable { },
+                                    elevation = 10.dp
+                                ) {
+                                    Column(
+                                        modifier = Modifier.padding(15.dp)
+                                    ) {
+                                        Text(
+                                            ("C Quiz \n" +
+                                                    " % \n" +
+                                                    " \n" +
+                                                    "  ")
+                                        )
+                                    }
+                                }
+                                Card(
+                                    backgroundColor = revBlue,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(15.dp)
+                                        .clickable { },
+                                    elevation = 10.dp
+                                ) {
+                                    Column(
+                                        modifier = Modifier.padding(15.dp)
+                                    ) {
+                                        Text(
+                                            ("C++ Quiz \n" +
+                                                    " % \n" +
+                                                    " \n" +
+                                                    "  ")
+                                        )
+                                    }
+                                }
+                                Card(
+                                    backgroundColor = revBlue,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(15.dp)
+                                        .clickable { },
+                                    elevation = 10.dp
+                                ) {
+                                    Column(
+                                        modifier = Modifier.padding(15.dp)
+                                    ) {
+                                        Text(
+                                            ("Kotlin \n" +
+                                                    " % \n" +
+                                                    " \n" +
+                                                    "  ")
+                                        )
+                                    }
+                                }
+                            }
                         }
-                    }
-                }
-
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(15.dp)
-                        .clickable { },
-                    elevation = 10.dp
-                ) {
-                    Column(
-                        modifier = Modifier.padding(15.dp)
-                    ) {
-                        Text(
-                            ("About me / subject interests \n" +
-                                    " \n" +
-                                    " \n" +
-                                    "  ")
-                        )
-                    }
-                }
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(15.dp)
-                        .clickable { },
-                    elevation = 10.dp
-                ) {
-                    Column(
-                        modifier = Modifier.padding(15.dp)
-                    ) {
-                        Text(
-                            ("C Quiz \n" +
-                                    " % \n" +
-                                    " \n" +
-                                    "  ")
-                        )
-                    }
-                }
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(15.dp)
-                        .clickable { },
-                    elevation = 10.dp
-                ) {
-                    Column(
-                        modifier = Modifier.padding(15.dp)
-                    ) {
-                        Text(
-                            ("C++ Quiz \n" +
-                                    " % \n" +
-                                    " \n" +
-                                    "  ")
-                        )
-                    }
-                }
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(15.dp)
-                        .clickable { },
-                    elevation = 10.dp
-                ) {
-                    Column(
-                        modifier = Modifier.padding(15.dp)
-                    ) {
-                        Text(
-                            ("Kotlin \n" +
-                                    " % \n" +
-                                    " \n" +
-                                    "  ")
-                        )
                     }
                 }
             }
         }
     }
 }
-
 
 @Preview
 @Composable
