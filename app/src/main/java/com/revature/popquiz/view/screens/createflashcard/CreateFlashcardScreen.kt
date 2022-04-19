d79acd5068ec7348c43ff483df1f2f6328e6078d
Evan Jones <eljones02@student.ysu.edu>
2022-04-19 11:14:33 -0400

    CreateFlashcard

---
+++ app/src/main/java/com/revature/popquiz/view/screens/createflashcard/CreateFlashcardScreen.kt
@@ -0,0 +1,9 @@
+package com.revature.popquiz.view.screens.createflashcard
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun CreateFlashcardScreen(navController: NavController) {
+    
+}
---
+++ app/src/main/java/com/revature/popquiz/view/screens/createflashcard/CreateFlashcardVM.kt
@@ -0,0 +1,4 @@
+package com.revature.popquiz.view.screens.createflashcard
+
+class CreateFlashcardVM {
+}
