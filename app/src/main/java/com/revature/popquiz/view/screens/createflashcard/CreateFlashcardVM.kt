bd3b7887e52cb46670552c62c47e14e7e58a2d50
Evan Jones <eljones02@student.ysu.edu>
2022-04-19 16:27:07 -0400

    Update CreateFlashcardVM.kt

--- app/src/main/java/com/revature/popquiz/view/screens/createflashcard/CreateFlashcardVM.kt
+++ app/src/main/java/com/revature/popquiz/view/screens/createflashcard/CreateFlashcardVM.kt
@@ -1,8 +1,10 @@
 package com.revature.popquiz.view.screens.createflashcard
 
 import androidx.lifecycle.ViewModel
+import com.revature.popquiz.model.dataobjects.Flashcard
 
 class CreateFlashcardVM: ViewModel() {
 
-
+    //new Flashcard
+    val newFlashcard: Flashcard = Flashcard()
 }
