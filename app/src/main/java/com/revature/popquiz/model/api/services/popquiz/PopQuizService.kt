a620b70fcd0b9ed5f955035914a7e8977832db28
Evan Jones <eljones02@student.ysu.edu>
2022-04-22 17:37:22 -0400

    Update PopQuizService.kt

--- app/src/main/java/com/revature/popquiz/model/api/services/popquiz/PopQuizService.kt
+++ app/src/main/java/com/revature/popquiz/model/api/services/popquiz/PopQuizService.kt
@@ -16,12 +16,11 @@ import android.content.Intent
 @SuppressLint("UnspecifiedImmutableFlag")
 fun setAlarm(context: Context) {
     /**
-     * CURRENTLY this will set an alarm for 5000 milliseconds in the future.
      * Allow users to set an alarm for a specific time in the day,
      * set multiple alarms at intervals? 30 minutes?
      */
 //    val time = Calendar.getInstance().set(Calendar.HOUR_OF_DAY, 12)
-    val time = System.currentTimeMillis() + 5000
+    val time = AlarmManager.INTERVAL_HALF_HOUR
     val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
     val intent = Intent(context, PopQuizReceiver::class.java)
     val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
