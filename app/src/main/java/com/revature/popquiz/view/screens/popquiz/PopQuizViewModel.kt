78933546b9cf864506d0dc08325ab26b17c9b2e9
Evan Jones <eljones02@student.ysu.edu>
2022-04-22 11:17:16 -0400

    Update PopQuizViewModel.kt

--- app/src/main/java/com/revature/popquiz/view/screens/popquiz/PopQuizViewModel.kt
+++ app/src/main/java/com/revature/popquiz/view/screens/popquiz/PopQuizViewModel.kt
@@ -32,85 +32,92 @@ class PopQuizViewModel: ViewModel() {
         get() = _state
 
     init {
-//        createNotificationChannel(context = Graph.appContext)
+        createNotificationChannel(context = Graph.appContext)
+        setOneTimeNotification()
         viewModelScope.launch {
             popQuizRepository.getTopic()
         }
     }
-}
 
-//With this method you can set the constraints required for the notification to show
-private fun setOneTimeNotification() {
-    val workManager = WorkManager.getInstance(Graph.appContext)
-    val constraints = Constraints.Builder()
-        .setRequiredNetworkType(NetworkType.CONNECTED)
-        .build()
-    val notificationWorker = OneTimeWorkRequestBuilder<NotificationWorker>()
-        .setInitialDelay(10, TimeUnit.SECONDS)
-        .setConstraints(constraints)
-        .build()
 
-    //add the notification to be "worked on" in the background
-    workManager.enqueue(notificationWorker)
+    //With this method you can set the constraints required for the notification to show
+    private fun setOneTimeNotification() {
+        val workManager = WorkManager.getInstance(Graph.appContext)
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        val notificationWorker = OneTimeWorkRequestBuilder<NotificationWorker>()
+            .setInitialDelay(10, TimeUnit.SECONDS)
+            .setConstraints(constraints)
+            .build()
 
-    //monitor the state of the worker
-    workManager.getWorkInfoByIdLiveData(notificationWorker.id)
-        .observeForever { workInfo ->
-            if (workInfo.state == WorkInfo.State.SUCCEEDED) {
-                createSuccessNotification()
-            } else {
-                createErrorNotification()
+        //add the notification to be "worked on" in the background
+        workManager.enqueue(notificationWorker)
+
+        //monitor the state of the worker
+        workManager.getWorkInfoByIdLiveData(notificationWorker.id)
+            .observeForever { workInfo ->
+                if (workInfo.state == WorkInfo.State.SUCCEEDED) {
+                    createSuccessNotification()
+                } else {
+                    createErrorNotification()
+                }
             }
-        }
-}
-
-fun createSuccessNotification() {
-    val notificationId = 1
-    val builder = NotificationCompat.Builder(Graph.appContext, "CHANNEL_ID")   //TODO create a companion object to store CHANNEL_ID
-        .setSmallIcon(R.drawable.rev_logo_2)
-        .setContentTitle("Notification Successful")
-        .setContentText("Pop!Quiz time")
-        .setPriority(NotificationCompat.PRIORITY_HIGH)
-
-    with(NotificationManagerCompat.from(Graph.appContext)) {
-        //Notification is unique for each notification defined, 1 is success
-        notify(notificationId, builder.build())
     }
-}
 
-fun createErrorNotification() {
-    val notificationId = 2
-    val builder = NotificationCompat.Builder(
-        Graph.appContext,
-        "CHANNEL_ID"
-    )   //TODO create a companion object to store CHANNEL_ID
-        .setSmallIcon(R.drawable.rev_logo_2)
-        .setContentTitle("Notification Failed")
-        .setContentText("no Pop!Quiz")
-        .setPriority(NotificationCompat.PRIORITY_HIGH)
+    private fun createSuccessNotification() {
+        val notificationId = 1
+        val builder = NotificationCompat.Builder(
+            Graph.appContext,
+            "CHANNEL_ID"
+        )   //TODO create a companion object to store CHANNEL_ID
+            .setSmallIcon(R.drawable.rev_logo_2)
+            .setContentTitle("Notification Successful")
+            .setContentText("Pop!Quiz time")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
 
-    with(NotificationManagerCompat.from(Graph.appContext)) {
-        //Notification is unique for each notification defined, 2 is failure
-        notify(notificationId, builder.build())
-    }
-}
-fun createNotificationChannel(context: Context) {
-
-    //Create the NotificationChannel
-    //This is only available in SDK Version 26+
-    /**
-     * Higher notification importance: shows everywhere, makes noise and peeks. May use full screen intents.
-     */
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        val notificationChannel = "NotificationChannelName"
-        val descriptionText = "NotificationChannelDescriptionText"
-        val importance = IMPORTANCE_HIGH
-        val channel = NotificationChannel("CHANNEL_ID", notificationChannel, importance).apply {
-            description = descriptionText
+        with(NotificationManagerCompat.from(Graph.appContext)) {
+            //Notification is unique for each notification defined, 1 is success
+            notify(notificationId, builder.build())
         }
-        //Register the channel with the system
-        val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun createErrorNotification() {
+        val notificationId = 2
+        val builder = NotificationCompat.Builder(
+            Graph.appContext,
+            "CHANNEL_ID"
+        )   //TODO create a companion object to store CHANNEL_ID
+            .setSmallIcon(R.drawable.rev_logo_2)
+            .setContentTitle("Notification Failed")
+            .setContentText("no Pop!Quiz")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+        with(NotificationManagerCompat.from(Graph.appContext)) {
+            //Notification is unique for each notification defined, 2 is failure
+            notify(notificationId, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel(context: Context) {
+
+        //Create the NotificationChannel
+        //This is only available in SDK Version 26+
+        /**
+         * Higher notification importance: shows everywhere, makes noise and peeks. May use full screen intents.
+         */
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = "NotificationChannelName"
+            val descriptionText = "NotificationChannelDescriptionText"
+            val importance = IMPORTANCE_HIGH
+            val channel = NotificationChannel("CHANNEL_ID", notificationChannel, importance).apply {
+                description = descriptionText
+            }
+            //Register the channel with the system
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
