6ab2ba67959682065730c883070ecfff2f12eb79
Evan Jones <eljones02@student.ysu.edu>
2022-04-22 15:27:43 -0400

    Definitions

---
+++ app/src/main/java/com/revature/popquiz/model/api/services/popquiz/PopQuizService.kt
@@ -0,0 +1,9 @@
+package com.revature.popquiz.model.api.services.popquiz
+
+/**
+ * foreground service saved in the user settings somewhere,
+ * the service should start a timer
+ * foreground service
+ * set up timer
+ * then connect to actual popquiz
+ */
--- app/src/main/java/com/revature/popquiz/view/screens/flashcard/FlashcardViewModel.kt
+++ app/src/main/java/com/revature/popquiz/view/screens/flashcard/FlashcardViewModel.kt
@@ -2,6 +2,10 @@ package com.revature.popquiz.view.screens.flashcard
 
 import androidx.lifecycle.ViewModel
 
+/**
+ * Store the quiz and whatever question we are on
+ *
+ */
 class FlashcardViewModel: ViewModel() {
 
 //    val apiService = RetrofitHelper.getFlashcardsService()
