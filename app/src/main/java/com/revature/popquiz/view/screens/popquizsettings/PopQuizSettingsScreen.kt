c0bbe145518719bcfd77fb4c35fea50164153601
Evan Jones <eljones02@student.ysu.edu>
2022-04-25 12:04:03 -0400

    Update PopQuizSettingsScreen.kt

--- app/src/main/java/com/revature/popquiz/view/screens/popquizsettings/PopQuizSettingsScreen.kt
+++ app/src/main/java/com/revature/popquiz/view/screens/popquizsettings/PopQuizSettingsScreen.kt
@@ -1,44 +1,78 @@
 package com.revature.popquiz.view.screens.popquizsettings
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.revature.popquiz.model.api.services.popquiz.setAlarm
 import com.revature.popquiz.view.shared.QuizScaffold
 
 @Composable
 fun PopQuizSettingsScreen(navController: NavController) {
-    val context = LocalContext.current
-    QuizScaffold(sTitle = "Pop!Quiz Settings", navController = navController) {
+    QuizScaffold(
+        sTitle = "Settings",
+        navController = navController
+    )
+    {
         Column(
             modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = "Set Alarm for 5 seconds",
-                modifier = Modifier.padding(10.dp),
-                fontSize = 16.sp
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Spacer(Modifier.size(10.dp))
+            Card(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .absolutePadding(
+                        top = 5.dp,
+                    ),
+                shape = AbsoluteRoundedCornerShape(
+                    topLeft = 20.dp,
+                    topRight = 20.dp
+                ),
+                elevation = 10.dp
             )
-            Button(onClick = {
-                setAlarm(context)
-            }) {
-                Text(text = "Set Alarm")
-            }
+            {
+                Column(
+                    modifier = Modifier.fillMaxSize(fraction = 0.9F),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                )
+                {
+                    Text(text = "POP QUIZ", fontSize = 30.sp,
+                        fontWeight = FontWeight.Medium)
+                    Spacer(modifier = Modifier.height(50.dp))
 
+                    Text(text = "Quiz Intervals", fontSize = 20.sp,
+                        fontWeight = FontWeight.Medium, modifier = Modifier.fillMaxWidth(0.9F)
+                        , textAlign = TextAlign.Left)
+
+                    Spacer(modifier = Modifier.height(30.dp))
+
+                    Text(text = "Quiz Length", fontSize = 20.sp,
+                        fontWeight = FontWeight.Medium, modifier = Modifier.fillMaxWidth(0.9F)
+                        , textAlign = TextAlign.Left)
+
+                    Spacer(modifier = Modifier.height(30.dp))
+
+                    Text(text = "Alert Type", fontSize = 20.sp,
+                        fontWeight = FontWeight.Medium, modifier = Modifier.fillMaxWidth(0.9F)
+                        , textAlign = TextAlign.Left)
+
+
+                }
+            }
         }
+
     }
 }
 
