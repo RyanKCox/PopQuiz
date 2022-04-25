103f31cc3483c33b65d1c735633ed8c7e06e096f
Evan Jones <eljones02@student.ysu.edu>
2022-04-25 12:27:07 -0400

    Update PopQuizSettingsScreen.kt

--- app/src/main/java/com/revature/popquiz/view/screens/popquizsettings/PopQuizSettingsScreen.kt
+++ app/src/main/java/com/revature/popquiz/view/screens/popquizsettings/PopQuizSettingsScreen.kt
@@ -1,23 +1,36 @@
 package com.revature.popquiz.view.screens.popquizsettings
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.revature.popquiz.service.INTENT_NOTIFICATION_ID
+import com.revature.popquiz.service.POP_QUIZ_NOTIFICATION_CHANNEL
+import com.revature.popquiz.view.screens.popquiz.PopQuizActivity
 import com.revature.popquiz.view.shared.QuizScaffold
 
 @Composable
 fun PopQuizSettingsScreen(navController: NavController) {
+
+    val context = LocalContext.current
+
     QuizScaffold(
         sTitle = "Settings",
         navController = navController
@@ -68,7 +81,42 @@ fun PopQuizSettingsScreen(navController: NavController) {
                         fontWeight = FontWeight.Medium, modifier = Modifier.fillMaxWidth(0.9F)
                         , textAlign = TextAlign.Left)
 
+                    Button(onClick = {
 
+                        val notifyIntent = Intent(
+                            context,
+                            PopQuizActivity::class.java
+                        ).apply {
+
+                            var flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        }
+
+                        val notifyPendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            PendingIntent.getActivity(
+                                context,
+                                INTENT_NOTIFICATION_ID,
+                                notifyIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                            )
+                        } else {
+                            TODO("VERSION.SDK_INT < M")
+                        }
+
+                        val builder = NotificationCompat.Builder(
+                            context,
+                            POP_QUIZ_NOTIFICATION_CHANNEL
+                        ).apply {
+
+                            setContentIntent(notifyPendingIntent)
+                        }
+
+                        with(NotificationManagerCompat.from(context)) {
+                            notify(INTENT_NOTIFICATION_ID, builder.build())
+                        }
+
+                    }) {
+
+                    }
                 }
             }
         }
