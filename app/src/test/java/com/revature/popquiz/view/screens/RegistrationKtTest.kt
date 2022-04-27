21d081aaa5a8f84efa97711e8558e22d192f31fb
jonathancastaneda-revature <jonathan741@revature.net>
2022-04-27 14:57:01 -0700

    added use of exit button on QuizFinished.kt

---
+++ app/src/test/java/com/revature/popquiz/view/screens/RegistrationKtTest.kt
@@ -0,0 +1,23 @@
+package com.revature.popquiz.view.screens
+
+import org.junit.Assert.*
+import org.junit.Before
+
+import org.junit.Test
+
+class RegistrationKtTest {
+    @Before
+    fun before()
+    {
+
+    }
+
+    @Test
+    fun checkParams() {
+        
+    }
+
+    @Test
+    fun checkMatch() {
+    }
+}
