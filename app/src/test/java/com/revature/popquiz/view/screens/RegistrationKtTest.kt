a7d797136e104933c4edd3efc0afd7d9a6e7b826
jonathancastaneda-revature <jonathan741@revature.net>
2022-04-28 07:23:17 -0700

    Registration unit test

--- app/src/test/java/com/revature/popquiz/view/screens/RegistrationKtTest.kt
+++ app/src/test/java/com/revature/popquiz/view/screens/RegistrationKtTest.kt
@@ -1,23 +1,41 @@
 package com.revature.popquiz.view.screens
 
+import com.revature.popquiz.checkMatch
+import com.revature.popquiz.checkParams
 import org.junit.Assert.*
 import org.junit.Before
 
 import org.junit.Test
 
 class RegistrationKtTest {
-    @Before
-    fun before()
+
+
+    @Test
+    fun `Checks if checkParams returns true if everything is filled`() {
+        val result = checkParams("Test","Test","Test","Test")
+        assertTrue(result)
+    }
+    @Test
+    fun `Checks if checkParams returns false if nothing is filled`() {
+        val result = checkParams("","","","")
+        assertFalse(result)
+    }
+    @Test
+    fun `Checks if checkParams returns false if one is not filled`() {
+        val result = checkParams("Test","Test","","Test")
+        assertFalse(result)
+    }
+    @Test
+    fun `Checks if checkMatch returns false if there is no match`()
     {
-
+        val result = checkMatch("Test","TestFail")
+        assertFalse(result)
     }
-
     @Test
-    fun checkParams() {
-        
+    fun `Checks if checkMatch returns true if there is a match`()
+    {
+        val result = checkMatch("Test","Test")
+        assertTrue(result)
     }
 
-    @Test
-    fun checkMatch() {
-    }
 }
